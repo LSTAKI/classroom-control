1d6fd9eab7a718846ce9c8463ed063d13d3ee924
TAKI ALTAF MULLA <takimulla2006@gmail.com>
2025-10-06 12:48:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1872405867-a6c48",
+  "appId": "1:90723360051:web:002604777e3bfbf16c3246",
+  "apiKey": "AIzaSyBnHiBoZO4JuYLgF_0-ufyvNn9qf6zM-I8",
+  "authDomain": "studio-1872405867-a6c48.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "90723360051"
+};

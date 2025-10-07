3c20e79eee2a2b5e162d20775500a08b4037d7fa
TAKI ALTAF MULLA <takimulla2006@gmail.com>
2025-10-07 05:50:05 +0000

    Try fixing this error: `Build Error: Export default doesn't exist in tar

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,12 +1,10 @@
 'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import firebase from '@genkit-ai/firebase';
 import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
   plugins: [
-    firebase(),
     googleAI(),
     googleCloud({
       // We recommend passing the project ID in as an environment

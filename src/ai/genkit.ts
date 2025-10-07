3ec84c6c75c585f38c5485c29422ffd92a19defa
TAKI ALTAF MULLA <takimulla2006@gmail.com>
2025-10-07 05:43:08 +0000

    also setup the ai logic from firebase

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,26 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {firebase} from '@genkit-ai/firebase';
+import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
+  plugins: [
+    firebase(),
+    googleAI(),
+    googleCloud({
+      // We recommend passing the project ID in as an environment
+      // variable.
+      projectId: process.env.GCLOUD_PROJECT,
+    }),
+  ],
+  // Log developer-friendly error messages and stack traces.
+  // NOTE: This should be disabled in production.
+  dev: {
+    logLevel: 'debug',
+  },
+  // We recommend using a production-grade logger in production.
+  logLevel: 'info',
+  // Allow Genkit to store traces and other data in a local file.
+  // NOTE: This should be disabled in production.
+  enableLocalFlowState: true,
 });
